b444580691f33626ff0828114c5d9b8589cf788b
Θεόδωρος Πλιάφας <43904719+Thodoros@users.noreply.github.com>
2018-10-27 12:32:12 +0300

    Update sound-menu-mouseover.js

--- sound-menu-mouseover.js
+++ sound-menu-mouseover.js
@@ -1,7 +1,7 @@
 function playclip() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all){
-  document.all.sound.src = "button-press.mp3";
+  document.all.sound.src = "button-click.mp3";
  }
 }
 else {
